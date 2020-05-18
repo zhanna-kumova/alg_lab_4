ccd3ba5579767877ab41c9b8a6cacab3ff84a5a8
frezirovschik <frezirovschik@DESKTOP-FPFUT5P>
2020-05-18 08:36:19 +0300

    Add project files.

---
+++ alg_lab_4/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace alg_lab_4
+{
+    partial class Lab4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.reset_btn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(12, 252);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(115, 48);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "Старт";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Location = new System.Drawing.Point(163, 252);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(115, 48);
+            this.stop_btn.TabIndex = 1;
+            this.stop_btn.Text = "Стоп";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+            // 
+            // reset_btn
+            // 
+            this.reset_btn.Location = new System.Drawing.Point(323, 252);
+            this.reset_btn.Name = "reset_btn";
+            this.reset_btn.Size = new System.Drawing.Size(115, 50);
+            this.reset_btn.TabIndex = 2;
+            this.reset_btn.Text = "Сброс";
+            this.reset_btn.UseVisualStyleBackColor = true;
+            this.reset_btn.Click += new System.EventHandler(this.reset_btn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(25, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(426, 186);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0:0:0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Lab4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 369);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.reset_btn);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.start_btn);
+            this.Name = "Lab4";
+            this.Text = "Секундомер";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Button reset_btn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ alg_lab_4/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alg_lab_4
+{
+    public partial class Lab4 : Form
+    {
+        public int milisec, sec, min;
+        public Lab4()
+        {
+            InitializeComponent();
+        }
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+        private void reset_btn_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            milisec = 0;
+            sec = 0;
+            min = 0;
+            timer1.Enabled = false;
+            label1.Text = "0:0:0";
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            milisec += 1;
+            if (milisec == 10) {
+                milisec = 0;
+                sec += 1;
+            }
+            if (sec == 60) {
+                sec = 0;
+                min += 1;
+            }
+            label1.Text = min + ":" + sec + ":" + milisec;
+        }
+    }
+}
